89d8c9e882014fe1115753218b650e66c3c2d931
fededomm <fededomm@outlook.it>
2024-01-12 16:07:22 +0100

    Refactor executeCommand function to redirect command output to stdout and stderr

--- main.go
+++ main.go
@@ -23,10 +23,11 @@ func main() {
 func executeCommand(label string) error {
 	command := fmt.Sprintf(`sudo NIXOS_LABEL="%s" nixos-rebuild switch`, label)
 	cmd := exec.Command("bash", "-c", command)
-	output, err := cmd.CombinedOutput()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("failed to execute command: %v", err)
 	}
-	fmt.Print(string(output))
 	return nil
 }
